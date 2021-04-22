7d12a0dd2f2c7b9120613e652f49c469904f8e63
Jason Knoll <jason.p.knoll@gmail.com>
2021-04-22 13:34:43 -0700

    Add school hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  "name" => "Desert Mountain High School",
+  "size" => "Large",
+  "number_of_students" => 2500,
+  "number_of_teachers" => 100,
+  "has_football" => true,
+  "has_library" => true,
+  "top_teachers" => ["Holland", "Mclean", "Page"],
+  "top_subjects" => ["Math", "Science", "History"]
+}

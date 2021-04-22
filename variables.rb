0ffebc99b82d769f5a297440a32d8d02605b2cb0
Jason Knoll <jason.p.knoll@gmail.com>
2021-04-22 13:01:04 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Marilyn Monroe"
+is_active = false
+famous_person = "Danny Devito"
+num_3 = 5
+num_9 = 23
+sum = num_9 + num_3
+animals = ["Zebra", "Fish", "Tiger"]
+animals.shift
+puts famous_person.size

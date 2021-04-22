0ba92f99d91fdea44ccc9238022efebf112f69ce
Jason Knoll <jason.p.knoll@gmail.com>
2021-04-22 13:35:23 -0700

    Add pet hash

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+pet = {
+  "name" => "Angel",
+  "breed" => "Great Pyrenees",
+  "walks_a_week" => 7,
+  "good_boy_points" => 10000000000,
+  "energetic" => false,
+  "protective" => true,
+  "favorite_toys" => ["Chew Toy", "Ball", "Stuffed Animal"],
+  "favorite_treats" => ["Greenies", "Biscuts", "Jerky"]
+}
+ 
